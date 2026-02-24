76034ff32a437b239338e78b4904f678b33c232d
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-02-24 16:03:39 +0100

    En este segundo commit se añaden dos mensajes

--- Proyecto/src/Paquete/Principal.java
+++ Proyecto/src/Paquete/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		System.out.println("Esta es la prueba final de control de versiones");
+		System.out.println("Se añade un mensaje por pantalla");
+		System.out.println("Se añade otro mensaje por pantalla");
 
 	}
 
